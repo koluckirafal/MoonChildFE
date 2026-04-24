f973357a74b1cd1e550f7cafbaa6b4ff8ef6960a
Rafał Kołucki <BluRaf@gmx.com>
2026-04-24 18:35:47 +0200

    fix: Lock to VSync on Dreamcast

--- Platform/Backends/Window/DreamcastWindow.cpp
+++ Platform/Backends/Window/DreamcastWindow.cpp
@@ -60,7 +60,7 @@ void DreamcastWindow::SetRelativeMouseMode(bool enabled)
 
 void DreamcastWindow::PumpOSEvents(IInput* sink, bool& outExitRequested)
 {
-    puts("NOOP: DreamcastWindow::PumpOSEvents");
+    // TODO: DreamcastWindow::PumpOSEvents
     /*
     SDL_Event sdlEvent;
     while (SDL_PollEvent(&sdlEvent))
